afe9dcfd84902c12b0285bf46ce84e66af6c6c18
Martin Pavlasek <tuzticzka@gmail.com>
2020-07-23 16:21:41 +0200

    add decorator for functions (real Python decorator syntax)

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -1,2 +1,9 @@
 def decorator(input_str):
     return f'({input_str})'
+
+
+def decorator_as_fn(fn):
+    def wrapper(*args, **kwargs):
+        value = fn(*args, **kwargs)
+        return f'({value})'
+    return wrapper
--- decorator/tests/test_decorator.py
+++ decorator/tests/test_decorator.py
@@ -1,10 +1,19 @@
-from decorator.decorator import decorator
+from decorator.decorator import decorator, decorator_as_fn
 from pytest import fixture
 
 
 @fixture
 def input_str():
     return 'abc'
+
+
+def undecorated(a, b):
+    return a + b
+
+
+@decorator_as_fn
+def decorated(a, b):
+    return undecorated(a, b)
 
 
 def test_returned_instance_has_same_subtype_as_original(input_str):
@@ -21,3 +30,12 @@ def test_decorated_text(input_str):
 
 def test_multiple_decorated_text(input_str):
     assert decorator(decorator(input_str)) == '((abc))'
+
+
+def test_decorator_for_function():
+    assert undecorated(10, 2) == 12
+
+
+def test_decorator_for_function():
+    assert decorated(10, 2) == '(12)'
+    assert decorated('tu', 'kabel') == '(tukabel)'
